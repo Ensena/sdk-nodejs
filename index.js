34daa410e5d6f1af087b2d61b8961ac4367c396a
elmalba <5631542+elmalba@users.noreply.github.com>
2020-05-19 23:02:45 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,45 @@
+const fetch = require('node-fetch');
+
+function doRequest(url) {
+  return new Promise( (resolve, reject) => {
+    fetch(url)
+    .then(res => res.json())
+    .then(json => {resolve(json) } );
+  });
+}
+
+let getUser = async ()=> {
+  const res =  doRequest('https://api.xn--ensea-rta.cl/external/v1/getUser');
+  return res;
+}
+
+let saveUser = async ()=> {
+  const res =  doRequest('https://api.xn--ensea-rta.cl/external/v1/saveUser');
+  return res;
+}
+let NotificateUser = async ()=> {
+  const res =  doRequest('https://api.xn--ensea-rta.cl/external/v1/saveUser');
+  return res;
+}
+
+let getUsers = async ()=> {
+  const res =  doRequest('https://api.xn--ensea-rta.cl/external/v1/getUsers');
+  return res;
+}
+
+let getApp = async ()=> {
+  const res =  doRequest('https://api.xn--ensea-rta.cl/external/v1/getApp');
+  return res;
+}
+let saveAppSetting = async ()=> {
+  const res =  doRequest('https://api.xn--ensea-rta.cl/external/v1/getApp');
+  return res;
+}
+
+
+module.exports = {getUser:getUser,
+                  getUsers:getUsers,
+                  getApp:getApp,
+                  saveUser:saveUser,
+                  NotificateUser:NotificateUser,
+                  saveAppSetting:saveAppSetting};

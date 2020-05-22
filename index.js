b2ff106b06a44ddd67589a7f401966dcd0a74709
Manuel Alba <malba@Manuels-MacBook-Pro.local>
2020-05-22 02:32:02 -0400

    more methods

--- index.js
+++ index.js
@@ -1,45 +1,67 @@
 const fetch = require('node-fetch');
+let Ensena_key = ""
+let User_key = ""
 
 function doRequest(url) {
-  return new Promise( (resolve, reject) => {
+  return new Promise((resolve, reject) => {
     fetch(url)
-    .then(res => res.json())
-    .then(json => {resolve(json) } );
+      .then(res => res.json())
+      .then(json => { resolve(json) });
   });
 }
 
-let getUser = async ()=> {
-  const res =  doRequest('https://api.xn--ensea-rta.cl/external/v1/user');
+let getUser = async (user,requestuser) => {
+  const res = doRequest('https://api.xn--ensea-rta.cl/external/v1/user');
   return res;
 }
 
-let saveUser = async ()=> {
-  const res =  doRequest('https://api.xn--ensea-rta.cl/external/v1/user');
+let saveUser = async () => {
+  const res = doRequest('https://api.xn--ensea-rta.cl/external/v1/user');
   return res;
 }
-let notificateUser = async ()=> {
-  const res =  doRequest('https://api.xn--ensea-rta.cl/external/v1/notificateUser');
-  return res;
-}
-
-let getUsers = async ()=> {
-  const res =  doRequest('https://api.xn--ensea-rta.cl/external/v1/users');
+let notificateUser = async () => {
+  const res = doRequest('https://api.xn--ensea-rta.cl/external/v1/notificateUser');
   return res;
 }
 
-let getApp = async ()=> {
-  const res =  doRequest('https://api.xn--ensea-rta.cl/external/v1/app');
-  return res;
-}
-let saveAppSetting = async ()=> {
-  const res =  doRequest('https://api.xn--ensea-rta.cl/external/v1/app');
+let getUsers = async () => {
+  const res = doRequest('https://api.xn--ensea-rta.cl/external/v1/users');
   return res;
 }
 
+let getApp = async () => {
+  const res = doRequest('https://api.xn--ensea-rta.cl/external/v1/app');
+  return res;
+}
+let saveAppSetting = async () => {
+  const res = doRequest('https://api.xn--ensea-rta.cl/external/v1/app');
+  return res;
+}
 
-module.exports = {getUser:getUser,
-                  getUsers:getUsers,
-                  getApp:getApp,
-                  saveUser:saveUser,
-                  notificateUser:notificateUser,
-                  saveAppSetting:saveAppSetting};
+let SetApp = (key) => {
+  Ensena_key = key
+}
+let SetWebUser = () => {
+  Ensena_key = key
+  let url_string = window.location.href
+  let url = new URL(url_string);
+  User_key = url.searchParams.get("TOKEN");
+  getUser().then((data)=>{ 
+    if (data.Status){
+      console.log("ERROR MSG : ",data.Msg)
+    }
+
+   })
+}
+
+
+module.exports = {
+  SetApp: SetApp,
+  SetWebUser: SetWebUser,
+  getUser: getUser,
+  getUsers: getUsers,
+  getApp: getApp,
+  saveUser: saveUser,
+  notificateUser: notificateUser,
+  saveAppSetting: saveAppSetting
+};
